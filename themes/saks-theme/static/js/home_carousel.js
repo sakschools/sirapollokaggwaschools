ba10801e0c176c7448011c30a3f62233dcfe250a
Timothy Ssewanyana <timothycollins256@gmail.com>
2024-03-18 16:05:05 +0300

    Removed thumbnails on carasoul

--- themes/saks-theme/static/js/home_carousel.js
+++ themes/saks-theme/static/js/home_carousel.js
@@ -1,49 +1,43 @@
-//step 1: get DOM
+// Step 1: Get DOM
 let nextDom = document.getElementById('next');
 let prevDom = document.getElementById('prev');
-
 let carouselDom = document.querySelector('.carousel');
 let SliderDom = carouselDom.querySelector('.carousel .list');
-let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
-let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
-let timeDom = document.querySelector('.carousel .time');
 
-thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-let timeRunning = 3000;
 let timeAutoNext = 7000;
 
-nextDom.onclick = function(){
-    showSlider('next');    
+nextDom.onclick = function () {
+    showSlider('next');
 }
 
-prevDom.onclick = function(){
-    showSlider('prev');    
+prevDom.onclick = function () {
+    showSlider('prev');
 }
+
 let runTimeOut;
 let runNextAuto = setTimeout(() => {
-    next.click();
+    nextDom.click(); // Corrected from next.click() to nextDom.click()
 }, timeAutoNext)
-function showSlider(type){
-    let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
-    let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
-    
-    if(type === 'next'){
+
+function showSlider(type) {
+    let SliderItemsDom = SliderDom.children;
+
+    if (type === 'next') {
         SliderDom.appendChild(SliderItemsDom[0]);
-        thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
         carouselDom.classList.add('next');
-    }else{
+    } else {
         SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
-        thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
         carouselDom.classList.add('prev');
     }
+
     clearTimeout(runTimeOut);
     runTimeOut = setTimeout(() => {
         carouselDom.classList.remove('next');
         carouselDom.classList.remove('prev');
-    }, timeRunning);
+    }, 300);
 
     clearTimeout(runNextAuto);
     runNextAuto = setTimeout(() => {
-        next.click();
+        nextDom.click(); // Corrected from next.click() to nextDom.click()
     }, timeAutoNext)
 }
